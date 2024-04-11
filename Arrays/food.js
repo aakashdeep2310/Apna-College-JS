1816294f3faed47f50de5ca57cbeb5f2f2de7e76
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-11 23:22:24 +0530

    updated

---
+++ Arrays/food.js
@@ -0,0 +1,20 @@
+let foodItems = ["Litch", "Mango", "Chips", "kurkure"];
+
+// foodItems.push("Banana")
+// console.log(foodItems);
+// foodItems.pop();
+// console.log(foodItems);
+// foodItems.toString();
+// console.log(foodItems);
+// console.log(typeof foodItems)
+
+// console.log(foodItems.toString());
+// console.log(typeof foodItems);
+
+// foodItems.unshift("Papaya");
+// console.log(foodItems);
+
+// console.log(foodItems.shift());
+
+
+console.log(foodItems.slice(1, 3));
---
+++ Arrays/practice.js
@@ -0,0 +1,20 @@
+let arr = [250, 645, 300, 900, 50];
+// let val;
+// for(let i = 0; i<arr.length; i++){
+//     val = arr[i] -(arr[i] * 0.1);
+//     arr[i] = val;
+
+    
+    
+// }
+
+let i = 0;
+for(let el of arr){
+    let offer = el-(el*0.1);
+    arr[i] = offer;
+    i++;
+}
+
+console.log(arr);
+
+
