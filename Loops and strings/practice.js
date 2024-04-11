c4529d553327e29631e49e69e35cf49c88aa67b9
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-11 11:29:59 +0530

    updated

---
+++ Loops and strings/practice.js
@@ -0,0 +1,8 @@
+let input = prompt("Enter the user name");
+
+// let userName = input.concat("@");
+// let userName;
+ let username = (`@${input}${input.length}`);
+ alert(username.trim());
+
+// prompt(userName);
