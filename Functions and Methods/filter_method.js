3f55143b6c32d6522212e05c006d199888e67a3f
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-12 11:56:32 +0530

    update

---
+++ Functions and Methods/filter_method.js
@@ -0,0 +1,12 @@
+// 2nd Array Method
+// Filter Method
+//  Creates a new array of elements that give true for a condition/filter.
+// Eg: all even elements
+
+//syntx
+let newArr = arr.filter((val) => {
+    return val % 2 == 0 // if return true value will store in new Array else not
+
+})
+
+ 
